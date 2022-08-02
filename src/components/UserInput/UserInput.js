69374acdc194edd008cfda3f94e1e555a085e60d
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-02 14:48:24 -0300

    Added func to modal

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,21 +1,25 @@
 import { useRef, useState } from "react";
 import Modal from "../Modal/Modal";
 
-const UserInput = () => {
+const UserInput = (props) => {
   const userInputRef = useRef();
   const [clickedInput, setClickedInput] = useState(false);
 
-  const addUserInputHandler = (props) => {
+  const addUserInputHandler = (event) => {
+    event.preventDefault();
     const userInputEntered = userInputRef.current.value;
-    setClickedInput(true);
-  };
+    setClickedInput(userInputEntered);
 
+    props.onUserInput(userInputEntered);
+  };
   return (
     <>
-      {clickedInput && <Modal content="This is a content." />}
-      <label>Type here what you want to be displayed</label>
-      <input type="text" ref={userInputRef} />
-      <button onClick={addUserInputHandler}>Submit</button>
+      {clickedInput && <Modal content={clickedInput} />}
+      <form onSubmit={addUserInputHandler}>
+        <label>Type here what you want to be displayed</label>
+        <input type="text" ref={userInputRef} />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
